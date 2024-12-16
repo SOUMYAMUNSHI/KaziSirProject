937fc51be1e617102b1ffe2ffb38e39fd5e6cd04
Soumya Munshi <soumyamunsi@gmail.com>
2024-12-16 23:51:09 +0530

    add question is updated

--- pages/components/sub_components/question_input_option.php
+++ pages/components/sub_components/question_input_option.php
@@ -1,8 +1,20 @@
+<?php
+if (isset($_SESSION["usernme"])) {
+}
+?>
 <div id="options-form" class="hidden"> <!--This div is hidden, this is a trick to hold value-->
     <input id="question-subject" type="text" name="question_subject">
     <input id="question-chapter" type="text" name="question_chapter">
     <input id="question-topic" type="text" name="question_topic">
 </div>
+
+<?php
+include_once("../../../static/connection/pdo_connection.php"); //include PDO connection
+
+$subject_stmt = $pdo_conn->prepare("SELECT `SubCode`, `SubName` FROM `subject`"); //preparing ptatement
+$subject_stmt->execute(); //executing statement
+
+?>
 
 <div class="input-question">
 
@@ -10,26 +22,66 @@
     <div id="input-question" class="subject-input subject_input animate__animated">
         <p class="input-subject_heading">Select Subject</p>
         <select class="select-subject_option" name="input-chapter" id="input-subject">
-            <option value="subject">Subject</option>
-            <option value="chapter">Chapter</option>
+            <option value="">Select Subject</option>
+
+
+
+
+
+
+            <?php
+            while ($subjects = $subject_stmt->fetch()) {
+                //to view all subject as option
+            ?>
+                <option value="<?php echo $subjects["SubCode"]; //subject code as value 
+                                ?>">
+                    <?php echo $subjects["SubName"]; //view subject name 
+                    ?>
+                </option>
+            <?php
+            }
+            ?>
         </select>
         <button id="subject-button" class="subject-button">Select</button>
     </div>
+
+
+
+
+
+
+
+
+
+
+
 
     <div id="chapter-input" class="subject-input subject_chpter hidden animate__animated">
         <p class="input-subject_heading">Select chapter</p>
         <select class="select-subject_option" name="input-chapter" id="input-chapter">
             <option value="chapter">Chapter</option>
-            <option value="chapter">Chapter</option>
+
+            <!--The options will load here-->
+
         </select>
         <button id="chapter-button" class="subject-button">Select</button>
     </div>
+
+
+
+
+
+
+
+
 
     <div id="topic-input" class="subject-input subject_topic hidden animate__animated">
         <p class="input-subject_heading">Select Topic</p>
         <select class="select-subject_option" name="input-chapter" id="input-topic">
             <option value="chapter">Topic</option>
-            <option value="chapter">Chapter</option>
+
+            <!--The topics will load-->
+
         </select>
         <button id="topic-button" class="subject-button">Select</button>
     </div>
@@ -37,5 +89,39 @@
 
 </div>
 
+
+
+
+
+
+
+<script src="../script/JQuery/jquery-3.7.1.js"></script> <!--Addding Jquery-->
+
+
+
+
+
+
 <!--Adding related javascript for animation and other-->
 <script src="../script/question_input.js"></script>
+
+
+
+
+
+<!--script to dynamically load the drop down-->
+<script>
+    //To change the @option dropdown using @subject dropdown
+    $("#input-subject").change(function() /* This function will rul when the subject dropdown will change or any optoin is selected */ {
+        const subjectCode = $("#input-subject").val(); //fetching the value of the sbject dropdown
+        $("#input-chapter").load(`../pages/components/sub_components/chapter.php?subCode=${subjectCode}`); //sending the chapter code to the chapter.php
+    });
+
+
+    //to change @topic dropdoen using the @option dropdown
+
+    $("#input-chapter").change(function() {
+        const chapterCode = $("#input-chapter").val();
+        $("#input-topic").load(`../pages/components/sub_components/topic.php?chCode=${chapterCode}`);
+    })
+</script>
--- script/question_input.js
+++ script/question_input.js
@@ -15,18 +15,18 @@
         $("#topic-input").addClass("animate__bounceInRight");
 
         let input_chapter = $("#input-chapter").val(); //fetching value
-        $("#question-chapter").val("input_chapter"); //adding value
+        $("#question-chapter").val(input_chapter); //adding value
     });
 
     $("#topic-button").click(function() { //these all are for animation
 
         let input_topic = $("#input-topic").val(); //fetching value
-        $("#question-topic").val("input_topic"); //adding value
+        $("#question-topic").val(input_topic); //adding value
 
         const subject = $("#question-subject").val(); //fetching subject from hidden input
         const chapter = $("#question-chapter").val(); //fetching chapter from hidden input
         const topic = $("#question-topic").val(); //fetching topic from hidden topic
 
-        $(".view").load(`../pages/components/sub_components/input_question_header.php?subject=${subject}&chapter=${chapter}&topic=${topic}`); //sending value to input_question_header.php
+        $(".view").load(`../pages/components/sub_components/input_question_header.php?subject=${subject}&chapter=${chapter}&topic=${"ok"}`); //sending value to input_question_header.php
 
     });
